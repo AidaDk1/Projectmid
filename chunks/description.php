ca2b707dd1b5f4ca45814b36594cd2423e89d9a2
AidaDk1 <65682383+AidaDk1@users.noreply.github.com>
2021-03-21 15:10:15 +0600

    Add files via upload

---
+++ chunks/description.php
@@ -0,0 +1,16 @@
+<section class="fdes">
+		<div class="section white center">
+			<h2 class="header" style="padding:20px; padding-bottom: 30px;">Number 1 National Restaurant in Kyrgyzstan.Faster than you can imagine,
+			get food delivered at your door</h2>
+		      <div class="row container center">
+		        <div class="col center l8 s12">
+				<img height="300" width="auto" style="object-fit: contain;" src="images/plov.jpg" alt="">
+		        	<p>This is the web application of  KyrgyzFood restaurant
+				,Where you can order any kind of food online.We deliver wide variety of foods.We welcome your appetite.Let our food satisfy your stomach</p>
+					</div>
+		        <div class="col center l4 s12">
+		        	<img height="300" width="auto" style="object-fit: contain;" src="images/banner10.jpg" alt="">
+		        </div>
+		        
+		      </div>
+	</section>
